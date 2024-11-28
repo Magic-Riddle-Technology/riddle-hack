1e9426c4c38916fb6dba4dda0a1168eb09ae1642
Godspower Anyaogu <gpanyaogu@gmail.com>
2024-11-28 12:59:54 +0000

    update db uri

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/chidi";
+  process.env.MONGODB_URI || "mongodb://localhost:27017/riddle";
 
 export async function initializeDB() {
   return mongoose.connect(MONGODB_URI).then(() => {
